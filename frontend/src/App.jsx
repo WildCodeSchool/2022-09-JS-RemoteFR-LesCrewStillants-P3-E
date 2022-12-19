222c781dbf688e91101dd48d839fa699a7807b2c
Elie Parthenay <elie.parthenay@gmail.com>
2022-12-19 17:11:13 +0100

    Create login page and style, install react-router-dom AND fontawesome module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "../assets/css/login.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import loginLogo from "../assets/images/login-logo.jpg";
+import loginLogo2 from "../assets/images/login-logo2.jpg";
+import loginExemple from "../assets/images/login-exemple.jpg";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+
+  const changeEmail = (e) => {
+    if (
+      (/^[0-9a-zA-Z@_-\w.]+$/.test(e.target.value) &&
+        e.target.value.length <= 50) ||
+      !e.target.value
+    ) {
+      setEmail(e.target.value);
+    }
+  };
+
+  return (
+    <main className="login">
+      <section className="login-left">
+        <div className="login-content">
+          <img alt="Noticia logo" src={loginLogo} />
+          <h1>Bienvenue dans la nouvelle communication</h1>
+          <p>
+            Votre outils pour une meilleure communication en interne nous avons
+            mis en place pour vous cette solution efficace et intuitive.
+          </p>
+          <img alt="dashboard exemple" src={loginExemple} />
+          <button type="button">Connectez-vous</button>
+        </div>
+      </section>
+
+      <section id="login-right" className="login-right">
+        <div className="login-content">
+          <img alt="Enedis Logo" src={loginLogo2} />
+          <h1>Connexion</h1>
+
+          <form>
+            <input
+              value={email}
+              onChange={changeEmail}
+              type="email"
+              placeholder="Adresse Email"
+            />
+            <input type="password" placeholder="Mot de passe" />
+            <input type="checkbox" /> Rester connecté
+            <button type="button">
+              <FontAwesomeIcon icon={faArrowRightToBracket} /> Connexion
+            </button>
+            <span className="login-forget">Mot de passe oublié</span>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
+}
