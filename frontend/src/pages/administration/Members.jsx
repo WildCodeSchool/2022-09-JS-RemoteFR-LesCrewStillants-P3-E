30b8d53799ee96b92f474679810968c19b96aa8b
Elie Parthenay <elie.parthenay@gmail.com>
2023-01-17 12:36:00 +0100

    create modal component for add member

--- frontend/src/pages/administration/Members.jsx
+++ frontend/src/pages/administration/Members.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers, faAdd } from "@fortawesome/free-solid-svg-icons";
+import MemberAdd from "@components/admin/members/memberAdd";
 import nopicture from "../../assets/images/nopicture.png";
 import Header from "../../components/admin/Header";
 import NavLeft from "../../components/admin/NavLeft";
 import PageTitle from "../../components/admin/PageTitle";
 
 export default function Members({ fakeMembersList }) {
+  const [showModalAddMember, setShowModalAddMember] = useState(false);
+
+  const hundleClickAddMember = () => {
+    setShowModalAddMember(!showModalAddMember);
+  };
   return (
     <div className="admin">
+      {showModalAddMember ? (
+        <MemberAdd hundleClickAddMember={hundleClickAddMember} />
+      ) : null}
+
       <NavLeft elemActive="members" />
       <main>
         <Header />
         <div className="content">
           <PageTitle title="Membres" icon={faUsers} />
+          <button
+            onClick={hundleClickAddMember}
+            className="bg-lime-500 mb-10 text-base px-3 py-2 rounded text-white font-medium shadow-lg "
+            type="button"
+          >
+            <FontAwesomeIcon icon={faAdd} /> Add member
+          </button>
 
           <section className="membersList flex">
             {fakeMembersList.map((e) => (
