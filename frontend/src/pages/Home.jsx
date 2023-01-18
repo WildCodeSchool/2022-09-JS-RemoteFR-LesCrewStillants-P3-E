2cd79b650f2b740f965d4dc5169876dfe861520b
MUSTAPHA SAID <mustaphasaid.pr@gmail.com>
2023-01-18 17:12:03 +0100

    doing comment section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import PostBody from "@components/postbody/PostBody";
 import Header from "../components/header/Header";
+import "../components/postbody/maison.css";
 
 export default function Home() {
+  const [posts, setposts] = useState([]);
   return (
     <div>
       <Header />
+      <div className="body">
+        <div className="left">
+          <h1>divd,cn</h1>
+        </div>
+        <div className="middle">
+          {posts.map((post) => (
+            <PostBody />
+          ))}
+          <PostBody
+            name="Elie Partenay"
+            description="directeur admin"
+            message="Le fichier de prévention pour l’année 2023 est sorti. Il est nécessaire
+        que celui-ci soit à jour pour permettre une meilleure réactivité lors
+        d’un incident. Je compte sur vous pour vérifier et modifier les
+        informations si nécessaire"
+            date="6h"
+          />
+        </div>
+        <div className="right">
+          <h1>hello</h1>
+        </div>
+      </div>
     </div>
   );
 }
