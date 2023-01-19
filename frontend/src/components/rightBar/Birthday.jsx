d25151367fccf209704e2b89d98f0ae4192ee93d
mlgs <mathieu.langlois@voxm.live>
2023-01-19 10:03:02 +0100

    right bar done

--- frontend/src/components/rightBar/Birthday.jsx
+++ frontend/src/components/rightBar/Birthday.jsx
@@ -1,12 +1,11 @@
 import "../leftBar/ItemBar.css";
-import birthdayImg from "../../../public/img/birthday-img.png";
 
 export default function Birthday() {
   return (
     <div className="birthdayContainer">
-      <img src={birthdayImg} alt="" className="birthdayImg" />
       <span className="birthdayText">
         Happy birthday
+        <br />
         <b>Pola Foster</b>
       </span>
     </div>
